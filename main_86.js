f531a723b8315dc2e2c9b27c981c0db444326c1e
PatrickCodeing <117385304+PatrickCodeing@users.noreply.github.com>
2023-01-17 17:14:01 +0000

    Add files via upload

--- main_86.js
+++ main_86.js
@@ -2,8 +2,8 @@ var grades = [];
         var update_scores = function () {
            var val = get_item_list(grades);
            //Add the appropriate id names to read scores, student_name
-           document.getElementById("score").value = "";
-           document.getElementById("student_name").value = val;
+           document.getElementById("scores").value = val;
+           document.getElementById("student_name").value = "";
            document.getElementById("score").value = "";   
            document.getElementById("student_name").focus();
         }    
